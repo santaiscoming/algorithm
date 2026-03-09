867d594d8f33cfbfafea199b665c4de165190e14
santaiscoming <santaiscoming123123@gmail.com>
2026-03-09 20:43:13 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 4 ms, Memory: 14572 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.rs
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.rs
@@ -12,15 +12,16 @@ fn main() {
     let mut l = 0;
     let mut r = 0;
     let mut result = 0;
-    let mut set = HashSet::new();
+    let mut set = vec![false; 100001];
+
     while l < n {
-        while r < n && !set.contains(&nums[r]) {
-            set.insert(nums[r]);
+        while r < n && !set[nums[r]] {
+            set[nums[r]] = true;
             r += 1;
         }
 
         result += r - l;
-        set.remove(&nums[l]);
+        set[nums[l]] = false;
         l += 1;
     }
 
