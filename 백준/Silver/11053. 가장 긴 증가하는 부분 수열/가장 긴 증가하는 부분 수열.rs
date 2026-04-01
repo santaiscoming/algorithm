2c47d867cf7376b19646a4571d24165b02f52d2b
santaiscoming <santaiscoming123123@gmail.com>
2026-04-01 19:11:30 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 4 ms, Memory: 13200 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.rs
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.rs
@@ -3,6 +3,20 @@ use std::{
     io::{self, Read},
 };
 
+// bottom-up
+// let mut dp = vec![1; n];
+// for i in 1..n {
+//     let cur = arr[i];
+//     for j in (0..=i - 1).rev() {
+//         let prev = arr[j];
+
+//         if cur > prev {
+//             dp[i] = dp[i].max(dp[j] + 1);
+//         }
+//     }
+// }
+// println!("{}", dp.iter().max().unwrap());
+
 fn main() {
     let mut tokens = read!();
     let n = next!(&mut tokens, usize);
@@ -10,19 +24,28 @@ fn main() {
         .map(|_| next!(&mut tokens, usize))
         .collect::<Vec<_>>();
 
-    let mut dp = vec![1; n];
-    for i in 1..n {
-        let cur = arr[i];
-        for j in (0..=i - 1).rev() {
-            let prev = arr[j];
+    let mut res = 0;
+    let mut dp = vec![-1; n];
+    for i in 0..n {
+        res = res.max(top_down(i, &arr, &mut dp));
+    }
+    println!("{}", res);
+}
 
-            if cur > prev {
-                dp[i] = dp[i].max(dp[j] + 1);
-            }
+fn top_down(i: usize, arr: &Vec<usize>, dp: &mut Vec<i32>) -> i32 {
+    if dp[i] != -1 {
+        return dp[i];
+    }
+
+    let mut ret = 1i32;
+    for j in 0..i {
+        if arr[i] > arr[j] {
+            ret = ret.max(top_down(j, arr, dp) + 1);
         }
     }
-    
-    println!("{}", dp.iter().max().unwrap());
+    dp[i] = ret;
+
+    dp[i]
 }
 
 #[macro_export]
