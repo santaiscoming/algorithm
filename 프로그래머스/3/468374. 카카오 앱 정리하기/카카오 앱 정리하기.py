56cf89246c867cc6d68b6f1f56182753ee7d436b
santaiscoming <santaiscoming123123@gmail.com>
2026-05-13 23:32:16 +0900

    [level 3] Title: 카카오 앱 정리하기, Time: 812.90 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/3/468374. 카카오 앱 정리하기/카카오 앱 정리하기.py
+++ 프로그래머스/3/468374. 카카오 앱 정리하기/카카오 앱 정리하기.py
@@ -1,172 +1,86 @@
 from collections import deque
-from math import isqrt
-
 
 def solution(board, commands):
     n = len(board)
     m = len(board[0])
-
-    DIRS = [
-        None,
-        (0, 1),
-        (1, 0),
-        (0, -1),
-        (-1, 0),
-    ]
-
-    max_id = max(max(row) for row in board)
-
-    size = [0] * (max_id + 1)
-    pos = [None] * (max_id + 1)
-    count = [0] * (max_id + 1)
-    min_r = [10**9] * (max_id + 1)
-    min_c = [10**9] * (max_id + 1)
-
-    for r in range(n):
-        for c in range(m):
-            app_id = board[r][c]
-
-            if app_id == 0:
-                continue
-
-            count[app_id] += 1
-            min_r[app_id] = min(min_r[app_id], r)
-            min_c[app_id] = min(min_c[app_id], c)
-
-    app_ids = []
-
-    for app_id in range(1, max_id + 1):
-        if count[app_id] == 0:
-            continue
-
-        app_ids.append(app_id)
-
-        size[app_id] = isqrt(count[app_id])
-        pos[app_id] = (min_r[app_id], min_c[app_id])
-
-    def draw_board():
-        new_board = [[0] * m for _ in range(n)]
-
-        for app_id in app_ids:
-            r, c = pos[app_id]
-            s = size[app_id]
-
-            for i in range(s):
-                for j in range(s):
-                    nr = (r + i) % n
-                    nc = (c + j) % m
-                    new_board[nr][nc] = app_id
-
-        return new_board
-
-    cur_board = draw_board()
-
-    def get_front_cells(app_id, arrow):
-        """
-        app_id 앱을 arrow 방향으로 한 칸 밀 때
-        새로 차지하게 되는 앞쪽 한 줄의 칸들을 반환한다.
-        """
-        r, c = pos[app_id]
-        s = size[app_id]
-
-        if arrow == 1:
-            nc = (c + s) % m
-            return [((r + i) % n, nc) for i in range(s)]
-
-        if arrow == 2:
-            nr = (r + s) % n
-            return [(nr, (c + j) % m) for j in range(s)]
-
-        if arrow == 3:
-            nc = (c - 1) % m
-            return [((r + i) % n, nc) for i in range(s)]
-
-        nr = (r - 1) % n
-        return [(nr, (c + j) % m) for j in range(s)]
-
-    def get_push_group(start_id, arrow):
-        group = {start_id}
-        q = deque([start_id])
-
+    DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    
+    def get_dup_ids(id, arrow):
+        q = deque([id])
+        ret = set([id])
+        
         while q:
-            cur_id = q.popleft()
-
-            for r, c in get_front_cells(cur_id, arrow):
-                next_id = cur_board[r][c]
-
-                if next_id == 0:
-                    continue
-
-                if next_id in group:
-                    continue
-
-                group.add(next_id)
-                q.append(next_id)
-
-        return group
-
-    def move_group(group, arrow):
-        nonlocal cur_board
-
-        dr, dc = DIRS[arrow]
-
-        for app_id in group:
-            r, c = pos[app_id]
-            pos[app_id] = ((r + dr) % n, (c + dc) % m)
-
-        cur_board = draw_board()
-
-    def find_broken_app(arrow):
-        if arrow in (1, 3):
+            curr = q.popleft()
+            
             for r in range(n):
-                left_id = cur_board[r][0]
-                right_id = cur_board[r][m - 1]
+                for c in range(m):
+                    dr, dc = DIRS[arrow - 1]
+                    nr = (r + dr) % n
+                    nc = (c + dc) % m
+                    
+                    next = board[nr][nc]
+                    if (board[r][c] == curr and
+                        not next in ret and
+                        curr != next and
+                        next != 0):
+                        q.append(next)
+                        ret.add(next)
+                        
+        return ret
+    
+    def move_apps(ids, arrow):
+        dr, dc = DIRS[arrow - 1]
+        cells = []
+        for r in range(n):
+            for c in range(m):
+                if board[r][c] in ids:
+                    cells.append((r, c, board[r][c]))
 
-                if left_id == 0:
+        for r, c, id in cells:
+            board[r][c] = 0
+
+        for r, c, id in cells:
+            nr = (r + dr) % n
+            nc = (c + dc) % m
+            board[nr][nc] = id
+            
+    def get_broken_app(arrow):
+        if arrow in [1, 3]:
+            for r in range(n):
+                l_id = board[r][0]
+                r_id = board[r][m - 1]
+
+                if l_id == 0 or l_id != r_id:
                     continue
 
-                if left_id != right_id:
-                    continue
-
-                app_id = left_id
-                s = size[app_id]
-                _, c = pos[app_id]
-
-                if s < m and c + s > m:
-                    return app_id
-
+                is_broken = False
+                for c in range(m):
+                    if board[r][c] != l_id:
+                        return l_id
         else:
             for c in range(m):
-                top_id = cur_board[0][c]
-                bottom_id = cur_board[n - 1][c]
+                t = board[0][c]
+                b = board[n - 1][c]
 
-                if top_id == 0:
+                if t == 0 or t != b:
                     continue
 
-                if top_id != bottom_id:
-                    continue
-
-                app_id = top_id
-                s = size[app_id]
-                r, _ = pos[app_id]
-
-                if s < n and r + s > n:
-                    return app_id
+                for r in range(n):
+                    if board[r][c] != t:
+                        return t
 
         return None
-
-    for start_id, arrow in commands:
-        group = get_push_group(start_id, arrow)
-
-        move_group(group, arrow)
+    
+    for id, arrow in commands:
+        ids = get_dup_ids(id, arrow)
+        move_apps(ids, arrow)
 
         while True:
-            broken_id = find_broken_app(arrow)
-
-            if broken_id is None:
+            broken_app = get_broken_app(arrow)
+            if broken_app is None:
                 break
 
-            group = get_push_group(broken_id, arrow)
-            move_group(group, arrow)
+            ids = get_dup_ids(broken_app, arrow)
+            move_apps(ids, arrow)
 
-    return cur_board
+    return board
