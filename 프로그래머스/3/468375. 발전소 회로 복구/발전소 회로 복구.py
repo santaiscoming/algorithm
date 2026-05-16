46add2311a27f991a6f773cf284d1ae1199b54a2
santaiscoming <santaiscoming123123@gmail.com>
2026-05-16 21:42:38 +0900

    [level 3] Title: 발전소 회로 복구, Time: 673.49 ms, Memory: 20.7 MB -BaekjoonHub

--- 프로그래머스/3/468375. 발전소 회로 복구/발전소 회로 복구.py
+++ 프로그래머스/3/468375. 발전소 회로 복구/발전소 회로 복구.py
@@ -1,103 +1,96 @@
 from collections import deque
 
 def solution(h, grid, panels, seqs):
-    INF = 10 ** 15
-
+    INF = float('inf')
+    DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     n = len(grid)
     m = len(grid[0])
     k = len(panels)
-
-    DIRS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-
-    er = -1
-    ec = -1
+    
+    def bfs(r, c):
+        dist = [[-1] * m for _ in range(n)]
+        dist[r][c] = 0
+        
+        q = deque([(r, c)])
+        while q:
+            cr, cc = q.popleft()
+            
+            for dr, dc in DIRS: 
+                nr = cr + dr
+                nc = cc + dc
+                
+                if (0 <= nr < n and
+                    0 <= nc < m and
+                    dist[nr][nc] == -1 and
+                    grid[nr][nc] != '#'):
+                    dist[nr][nc] = dist[cr][cc] + 1
+                    q.append((nr, nc))
+            
+        return dist
+    
+    er = ec = None
     for r in range(n):
         for c in range(m):
             if grid[r][c] == '@':
-                er, ec = r, c
-    
+                er = r
+                ec = c
+                
     pos = [(er, ec)]
-    for f, r, c in panels:
+    for _, r, c in panels:
         pos.append((r - 1, c - 1))
-
-    floors = [0]  
-    for f, r, c in panels:
-        floors.append(f)
-
-    def bfs(start_r, start_c):
-        dist = [[-1] * m for _ in range(n)]
-        q = deque()
-
-        dist[start_r][start_c] = 0
-        q.append((start_r, start_c))
-
-        while q:
-            r, c = q.popleft()
-
-            for dr, dc in DIRS:
-                nr = r + dr
-                nc = c + dc
-
-                if nr < 0 or nr >= n or nc < 0 or nc >= m:
-                    continue
-
-                if grid[nr][nc] == '#':
-                    continue
-
-                if dist[nr][nc] != -1:
-                    continue
-
-                dist[nr][nc] = dist[r][c] + 1
-                q.append((nr, nc))
-
-        return dist
-
-    same_dist = [[0] * (k + 1) for _ in range(k + 1)]
-
+    
+    dist = [[-1] * (k + 1) for _ in range(k + 1)]
     for i in range(k + 1):
         sr, sc = pos[i]
-        d = bfs(sr, sc)
-
+        from_i = bfs(sr, sc)
         for j in range(k + 1):
             tr, tc = pos[j]
-            same_dist[i][j] = d[tr][tc]
-
-    def get_cost(a, b):
-        if floors[a] == floors[b]:
-            return same_dist[a][b]
-
-        return (
-            same_dist[a][0]
-            + abs(floors[a] - floors[b])
-            + same_dist[0][b]
-        )
-
-    pre = [0] * (k + 1)
+            dist[i][j] = from_i[tr][tc]
+    
+    floors = [0] * (k + 1)
+    for i, (f, _, _) in enumerate(panels):
+        floors[i + 1] = f
+        
+    def get_cost(i, j):
+        if floors[i] == floors[j]:
+            return dist[i][j]
+        
+        diff = abs(floors[i] - floors[j])
+        return dist[i][0] + diff + dist[0][j]
+        
+    
+    order = [0] * (k + 1)
     for a, b in seqs:
-        pre[b] |= 1 << (a - 1)
+        order[b] |= 1 << (a - 1)
 
     dp = [[INF] * (k + 1) for _ in range(1 << k)]
-    dp[0][1] = 0
-
+    mask = 0
+    start = 1
+    dp[mask][start] = 0
+    
+    # 가능한 모든 경우를 탐색해야하므로
     for mask in range(1 << k):
-        for pos in range(1, k + 1):
-            if dp[mask][pos] == INF:
+        for i in range(1, k + 1):
+            if dp[mask][i] == INF:
                 continue
 
             for nxt in range(1, k + 1):
                 bit = 1 << (nxt - 1)
+
                 if mask & bit:
                     continue
-                if (mask & pre[nxt]) != pre[nxt]:
+                if (mask & order[nxt]) != order[nxt]:
                     continue
 
                 next_mask = mask | bit
-                cost = get_cost(pos, nxt)
+                cost = get_cost(i, nxt)
 
                 dp[next_mask][nxt] = min(
                     dp[next_mask][nxt],
-                    dp[mask][pos] + cost
+                    dp[mask][i] + cost
                 )
-
+    
     full = (1 << k) - 1
-    return min(dp[full][pos] for pos in range(1, k + 1))
+    answer = min(dp[full][pos] for pos in range(1, k + 1))
+
+    return answer    
