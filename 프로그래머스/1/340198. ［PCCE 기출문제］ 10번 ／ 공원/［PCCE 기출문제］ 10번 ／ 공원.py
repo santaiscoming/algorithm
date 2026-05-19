7a76b0446744a9adefedc1386379f5c0b2842442
santaiscoming <santaiscoming123123@gmail.com>
2026-05-19 19:32:37 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 26.51 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -1,24 +1,21 @@
 def solution(mats, park):
-    h = len(park)
-    w = len(park[0])
-
-    mats.sort(reverse=True)
-
-    def can_place(r, c, size):
-        for i in range(r, r + size):
-            for j in range(c, c + size):
-                if park[i][j] != "-1":
-                    return False
-
-        return True
-
-    for size in mats:
-        if size > h or size > w:
-            continue
-
-        for r in range(h - size + 1):
-            for c in range(w - size + 1):
-                if can_place(r, c, size):
-                    return size
-
-    return -1
+    n = len(park)
+    m = len(park[0])
+    
+    candi = []
+    
+    for mat in mats:
+        for r in range(n):
+            for c in range(m):
+                if r + mat <= n and c + mat <= m:
+                    is_place = True
+                    for nr in range(r, r + mat):
+                        for nc in range(c, c + mat):
+                            if park[nr][nc] != "-1":
+                                is_place = False
+                                
+                    if is_place:
+                        candi.append(mat)
+                        
+                        
+    return -1 if not candi else max(candi)
