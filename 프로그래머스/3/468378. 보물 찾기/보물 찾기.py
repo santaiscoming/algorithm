a335e470c2e916e3c559e3a3f1ca53bdd15c9ef3
santaiscoming <santaiscoming123123@gmail.com>
2026-05-23 20:39:35 +0900

    [level 3] Title: 보물 찾기, Time: 725.29 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/3/468378. 보물 찾기/보물 찾기.py
+++ 프로그래머스/3/468378. 보물 찾기/보물 찾기.py
@@ -1,46 +1,44 @@
-import sys
-
 def solution(depth, money, excavate):
-    n = len(depth)
+    w = len(depth)
+    
+    depth = [0] + depth
+    memo = [[-1] * (w + 2) for _ in range(w + 2)]
+    nxt = [[0] * (w + 2) for _ in range(w + 2)]
+    
+    def recur(l, r):
+        if l > r:
+            return 0
+        
+        if memo[l][r] != -1:
+            return memo[l][r]
+        
+        min_cost = float('inf')
+        selec_k = l
+        
+        for k in range(l, r + 1):
+            cur_cost = depth[k] + max(recur(l, k - 1), recur(k + 1, r))
+            
+            if cur_cost < min_cost:
+                min_cost = cur_cost
+                selec_k = k
+                
+        nxt[l][r] = selec_k
+        memo[l][r] = min_cost
+        
+        return min_cost
 
-    INF = float('inf')
-    dp = [[0] * n for _ in range(n)]
-    picked = [[-1] * n for _ in range(n)]
-
-    for length in range(1, n + 1):
-        for l in range(n - length + 1):
-            r = l + length - 1
-
-            best = INF
-            best_k = -1
-
-            for k in range(l, r + 1):
-                left = dp[l][k - 1] if k > l else 0
-                right = dp[k + 1][r] if k < r else 0
-
-                cost = depth[k] + max(left, right)
-
-                if cost < best:
-                    best = cost
-                    best_k = k
-
-            dp[l][r] = best
-            picked[l][r] = best_k
-
-    l, r = 0, n - 1
-
+    recur(1, w)
+    
+    l, r = 1, w
     while l <= r:
-        k = picked[l][r]
-
-        res = excavate(k + 1)
-
+        k = nxt[l][r]
+        res = excavate(k)
+        
         if res == 0:
-            return k + 1
-
+            return k
         elif res == -1:
             r = k - 1
-
         else:
             l = k + 1
-
+            
     return -1
