2e839aff25526db4c8c2dc56de37a7b166289cc9
santaiscoming <santaiscoming123123@gmail.com>
2026-05-25 19:31:42 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.04 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
@@ -1,25 +1,25 @@
 def solution(n, w, num):
-    r, c = get_coords(num, w)
-    m_r, _ = get_coords(n, w)
-    
-    ans = 0
-    for r in range(r, m_r + 1):
-        if r % 2 == 0:
-            curr = r * w + c + 1
+    container = [[] for _ in range(w)]
+    v = 0
+    flag = True
+    while v < n:
+        if flag:
+            for i in range(w):
+                container[i].append(v)
+                v += 1
+                if v == n:
+                    break
+            flag = False
         else:
-            curr = r * w + (w - 1 - c) + 1
-            
-        if curr <= n:
-            ans += 1
-            
-    return ans
-
-def get_coords(num, w):
-        r = (num - 1) // w
-        rest = (num - 1) % w
+            for i in range(w - 1, -1, -1):
+                container[i].append(v)
+                v += 1
+                if v == n:
+                    break
+            flag = True
         
-        if r % 2 == 0:
-            col = rest
-        else:
-            col = (w - 1) - rest
-        return r, col
+    num = num - 1
+    for arr in container:
+        if num in arr:
+            i = arr.index(num)
+            return len(arr) - i
