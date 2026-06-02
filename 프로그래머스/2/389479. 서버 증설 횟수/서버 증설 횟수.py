e5ca71d724cae52af360ba7012b18de97aee1cf3
santaiscoming <santaiscoming123123@gmail.com>
2026-06-02 20:36:55 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.67 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
@@ -1,24 +1,23 @@
 from collections import deque
+
 def solution(players, m, k):
     ans = 0
     server = deque()
     
     for player in players:
-        curr_cap = (len(server) + 1) * m
-        
-        if player >= curr_cap:
-            inc = ((player - curr_cap) // m) + 1
-            ans += inc
-            for _ in range(inc):
-                server.append(k)
-            
-        remove = 0
-        for i, s in enumerate(server):
+        for i in range(len(server)):
             server[i] -= 1
-            if server[i] == 0:
-                remove += 1
-        
-        for _ in range(remove):
+            
+        while server and server[0] == 0:
             server.popleft()
-                    
+            
+        over = player - m
+        if over >= 0:
+            need = (over // m) + 1
+            if need > len(server):
+                req = need - len(server)
+                ans += req
+                server.extend([k] * req)
+                
+
     return ans
