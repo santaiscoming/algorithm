be17f9b8675fa575d3d6cdcd6c91f8738e6fd83f
santaiscoming <santaiscoming123123@gmail.com>
2026-06-19 23:56:16 +0900

    [level 2] Title: 숫자 변환하기, Time: 217.47 ms, Memory: 19.1 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -2,18 +2,16 @@ def solution(x, y, n):
     INF = float('inf')
     dp = [INF] * (y + 1)
     dp[x] = 0
-
-    for cur in range(x, y + 1):
-        if dp[cur] == INF:
+    
+    for v in range(x, y + 1):
+        if dp[v] == INF:
             continue
-
-        if cur + n <= y:
-            dp[cur + n] = min(dp[cur + n], dp[cur] + 1)
-
-        if cur * 2 <= y:
-            dp[cur * 2] = min(dp[cur * 2], dp[cur] + 1)
-
-        if cur * 3 <= y:
-            dp[cur * 3] = min(dp[cur * 3], dp[cur] + 1)
-
+            
+        if v + n <= y:
+            dp[v + n] = min(dp[v + n], dp[v] + 1)
+        if v * 2 <= y:
+            dp[v * 2] = min(dp[v * 2], dp[v] + 1)
+        if v * 3 <= y:
+            dp[v * 3] = min(dp[v * 3], dp[v] + 1)
+            
     return -1 if dp[y] == INF else dp[y]
