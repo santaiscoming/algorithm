414ee7e7c46f1933bf3c2feab7a276be80dc825e
santaiscoming <santaiscoming123123@gmail.com>
2026-06-21 20:48:32 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -1,21 +1,16 @@
 def solution(storey):
     ans = 0
-    
-    while storey > 0:
-        curr = storey % 10
-        next_d = (storey // 10) % 10
-        
-        if curr > 5:
-            ans += (10 - curr)
-            storey = (storey // 10) + 1
-        elif curr < 5:
-            ans += curr
-            storey //= 10
+
+    while storey:
+        cur = storey % 10
+        nxt = (storey // 10) % 10
+
+        if cur > 5 or (cur == 5 and nxt >= 5):
+            ans += 10 - cur
+            storey += 10
         else:
-            ans += 5
-            if next_d >= 5:
-                storey = (storey // 10) + 1
-            else:
-                storey //= 10
-                
+            ans += cur
+
+        storey //= 10
+
     return ans
