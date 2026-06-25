4033ae615a7e0dc20f7440e23bc5f9f184329b8f
santaiscoming <santaiscoming123123@gmail.com>
2026-06-25 20:58:39 +0900

    [level 2] Title: 택배상자, Time: 87.90 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -1,12 +1,20 @@
 def solution(order):
-    s = []
-    i = 0
+    n = len(order)
     
-    for box in range(1, len(order) + 1):
-        s.append(box)
-        
-        while s and s[-1] == order[i]:
-            s.pop()
-            i += 1
+    ans = 0
+    s = []
+    t = 0
+    
+    for v in range(1, n + 1):
+        if order[t] == v:
+            t += 1
+            ans += 1
+        else:
+            s.append(v)
             
-    return i
+        while s and order[t] == s[-1]:
+            s.pop()
+            ans += 1
+            t += 1
+        
+    return ans
